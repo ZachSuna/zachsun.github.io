fdb30e19140f5a1698fe0c278389b92127d7411f
ZULFIKAR ACHYAR <95424851+ZachSuna@users.noreply.github.com>
2023-05-16 03:01:54 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+const header = document.querySelector("header");
 
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open');
-};
-
-const sr = ScrollReveal ({
-    distance: '65px',
-    duration: 2600,
-    delay: 450,
-    reset: true
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 80);
 });
-
-sr.reveal('.kaanggo-text',{delay:200, origin:'top'});
-sr.reveal('.kaanggo-img',{delay:450, origin:'top'});
-sr.reveal('.icons',{delay:500, origin:'left'});
-sr.reveal('.scroll-down',{delay:500, origin:'right'});
